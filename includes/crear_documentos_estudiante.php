2160cb8fa3123b21eb63f12468f6b90461e2c471
Andres Malfavaun <2203024384@alumnos.cua.uam.mx>
2023-06-06 22:27:11 -0600

    p

--- includes/crear_documentos_estudiante.php
+++ includes/crear_documentos_estudiante.php
@@ -3,39 +3,39 @@ require_once('../controller/conect.php');
 
 if ($_POST) {
         // header("../index.php");
-        $matricula_estudiante = $_POST["matricula_estudiante"];
-        $ine = $matricula_estudiante.'_'. $_FILES['ine']["name"];
-        $ine_tmp = $_FILES['ine']["tmp_name"];
+        $matricula_estudiante = $_POST['matricula_estudiante'];
+        $ine = $matricula_estudiante.'_'. $_FILES['ine']['name'];
+        $ine_tmp = $_FILES['ine']['tmp_name'];
 
-        $comprobante_domicilio = $matricula_estudiante.'_'. $_FILES['comprobante_domicilio']["name"];
-        $comprobante_domicilio_tmp = $_FILES['comprobante_domicilio']["tmp_name"];
+        $comprobante_domicilio = $matricula_estudiante.'_'. $_FILES['comprobante_domicilio']['name'];
+        $comprobante_domicilio_tmp = $_FILES['comprobante_domicilio']['tmp_name'];
         
-        $curp = $matricula_estudiante.'_'. $_FILES['curp']["name"];
-        $curp_tmp = $_FILES['curp']["tmp_name"];
+        $curp = $matricula_estudiante.'_'. $_FILES['curp']['name'];
+        $curp_tmp = $_FILES['curp']['tmp_name'];
 
-        $seguro = $matricula_estudiante.'_'. $_FILES['seguro']["name"];
-        $seguro_tmp = $_FILES['seguro']["tmp_name"];
+        $seguro = $matricula_estudiante.'_'. $_FILES['seguro']['name'];
+        $seguro_tmp = $_FILES['seguro']['tmp_name'];
 
-        $cv = $matricula_estudiante.'_'. $_FILES['cv']["name"];
-        $cv_tmp = $_FILES['cv']["tmp_name"];
+        $cv = $matricula_estudiante.'_'. $_FILES['cv']['name'];
+        $cv_tmp = $_FILES['cv']['tmp_name'];
         
-        $constancia = $matricula_estudiante.'_'. $_FILES['constancia']["name"];
-        $constancia_tmp = $_FILES['constancia']["tmp_name"];
+        $constancia = $matricula_estudiante.'_'. $_FILES['constancia']['name'];
+        $constancia_tmp = $_FILES['constancia']['tmp_name'];
         
-        $carta_compromiso = $matricula_estudiante.'_'. $_FILES['carta_compromiso']["name"];
-        $carta_compromiso_tmp = $_FILES['carta_compromiso']["tmp_name"];
+        $carta_compromiso = $matricula_estudiante.'_'. $_FILES['carta_compromiso']['name'];
+        $carta_compromiso_tmp = $_FILES['carta_compromiso']['tmp_name'];
         
-        $formato_pago = $matricula_estudiante.'_'. $_FILES['formato_pago']["name"];
-        $formato_pago_tmp = $_FILES['formato_pago']["tmp_name"];
+        $formato_pago = $matricula_estudiante.'_'. $_FILES['formato_pago']['name'];
+        $formato_pago_tmp = $_FILES['formato_pago']['tmp_name'];
 
-        $cuenta_bancaria = $matricula_estudiante.'_'. $_FILES['cuenta_bancaria']["name"];
-        $cuenta_bancaria_tmp = $_FILES['cuenta_bancaria']["tmp_name"];
+        $cuenta_bancaria = $matricula_estudiante.'_'. $_FILES['cuenta_bancaria']['name'];
+        $cuenta_bancaria_tmp = $_FILES['cuenta_bancaria']['tmp_name'];
 
-        $formato_postulacion = $matricula_estudiante.'_'. $_FILES['formato_postulacion']["name"];
-        $formato_postulacion_tmp = $_FILES['formato_postulacion']["tmp_name"];
+        $formato_postulacion = $matricula_estudiante.'_'. $_FILES['formato_postulacion']['name'];
+        $formato_postulacion_tmp = $_FILES['formato_postulacion']['tmp_name'];
             
-            // $carpeta_documentos = "./files/";
-            $carpeta_documentos = __DIR__ . '/files/';;
+            // $carpeta_documentos = "/files/";
+            $carpeta_documentos = __DIR__ . '/files/';
             $archivo = $carpeta_documentos . basename($ine);
             $archivo2 = $carpeta_documentos . basename($curp);
             // echo $archivo;
